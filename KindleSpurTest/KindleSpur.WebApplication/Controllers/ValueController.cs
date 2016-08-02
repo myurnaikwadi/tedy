082d2054cdd0ee21af946648b255aad411805ff7
LWPCOMP26 <LWPCOMP26@LWPCOMP26>
2016-08-02 10:15:21 +0530

    ValueController added with story creation methods

--- KindleSpurTest/KindleSpur.WebApplication/Controllers/ValueController.cs
+++ KindleSpurTest/KindleSpur.WebApplication/Controllers/ValueController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using KindleSpur.Data;
+using KindleSpur.Models;
+using KindleSpur.Models.Interfaces;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +12,34 @@ namespace KindleSpur.WebApplication.Controllers
 {
     public class ValueController : Controller
     {
+        private readonly UserRepository _userRepo;
+        private readonly string UserId;
+        public ValueController()
+        {
+            UserId = ((IUser)System.Web.HttpContext.Current.Session["User"]).EmailAddress;
+            _userRepo = new UserRepository(UserId);
+        }
+
         // GET: Value
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public string SaveValueFeedStory(ValueFeedStory story)
+        {
+            ResponseMessage response = new ResponseMessage();
+            if(!_userRepo.SaveValueFeedStory(story))
+            {
+                 response.FailureCallBack("Unable to create your story");
+            }
+            return response.ToJson();
+        }
+
+        public string GetValueFeedStories(string ImpactZone)
+        {
+            return _userRepo.GetValueFeedStories(ImpactZone);
         }
     }
 }
