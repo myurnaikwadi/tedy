3f135d2a1ec40abbf20d6f43898b7e428f1cc047
piyushdayama <dayampiy@ARIEL>
2016-08-03 16:20:15 +0530

    Tushar: change EditCoacheeOrMentee(string UserId, ICoacheeOrMentee Data)

--- KindleSpurTest/KindleSpur.WebApplication/Scripts/Angular/Services/ksService.js
+++ KindleSpurTest/KindleSpur.WebApplication/Scripts/Angular/Services/ksService.js
@@ -154,7 +154,7 @@ app.factory('serverCommunication', function ($http) {
           */
         getMyMenteeSelection: function (iObj) {
             console.error(iObj)
-            $http.get('/Mentor/GetTopics').then(iObj.successCallBack, iObj.failureCallBack);
+            $http.get('/Mentee/GetTopics').then(iObj.successCallBack, iObj.failureCallBack);
         },
         /**
          * @auther : MKN
@@ -163,7 +163,7 @@ app.factory('serverCommunication', function ($http) {
          */
         getMyCoacheeSelection: function (iObj) {
             console.error(iObj)
-            $http.get('/Mentor/GetTopics').then(iObj.successCallBack, iObj.failureCallBack);
+            $http.get('/Coachee/GetTopics').then(iObj.successCallBack, iObj.failureCallBack);
         },
         /**
           * @auther : MKN
