e81e702eea1974afbbd7326f669135ea4e177491
LWPCOMP24 <LWPCOMP24@LWPCOMP24>
2016-08-07 13:08:17 +0530

    Email to be sent to users Activation account Forgot password

--- KindleSpurTest/KindleSpur.WebApplication/EmailHelper/EmailNotification.cs
+++ KindleSpurTest/KindleSpur.WebApplication/EmailHelper/EmailNotification.cs
@@ -9,10 +9,14 @@ namespace KindleSpur.WebApplication.MessageHelper
 {
     public class EmailNotification
     {
+        static string emailAddress = "pranav.suknale@livewireprojects.com";
+        static string password = "pranav2515";
+        static MailAddress aliasemailsendername = new MailAddress(emailAddress.ToString(), "KindleSpur Support Team");
 
         public static void SendEmail(User signupObject, string uri)
         {
-            MailMessage message = new MailMessage("proxyserver.myur@gmail.com", signupObject.EmailAddress);
+            
+            MailMessage message = new MailMessage(aliasemailsendername.ToString(), signupObject.EmailAddress);
             message.Subject = "Account Activation";
             string body = "Hello " + signupObject.FirstName + ",";
             body += "<br/><br/>Please click the following link to activate your account<br/>";
@@ -21,33 +25,50 @@ namespace KindleSpur.WebApplication.MessageHelper
             message.Body = body;
             message.IsBodyHtml = true;
             SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new System.Net.NetworkCredential("proxyserver.myur@gmail.com", "mayu3337sis");
+            smtp.Credentials = new System.Net.NetworkCredential(emailAddress.ToString(), password.ToString());
+            smtp.EnableSsl = true;
+            smtp.Send(message);
+        }
+
+        public static void SendEmailForgotPassword(User signupObject, string uri)
+        {
+           
+            MailMessage message = new MailMessage(aliasemailsendername.ToString(), signupObject.EmailAddress);
+            message.Subject = "Forgot Password";
+            string body = "Hello " + signupObject.FirstName + ",";
+            body += "<br/><br/>Please click the following link to enter new password<br/>";
+            body += "<br/><br/><a style='background:#000000; color:#fafafa; padding:10px 100px 10px 100px; width:350px; text-decoration:none; font-weight:bold; font-size:20px;' href = '" + uri + "'>Click here to enter new password.</a>";
+            body += "<br /><br />Thanks, <br/> KindleSpur Team.";
+            message.Body = body;
+            message.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.Credentials = new System.Net.NetworkCredential(emailAddress.ToString(), password.ToString());
             smtp.EnableSsl = true;
             smtp.Send(message);
         }
 
         public static void SendConversationEmail(Conversation _obj, string uri, string subject, string content)
         {
-            MailMessage message = new MailMessage("proxyserver.myur@gmail.com", _obj.ReceiverEmail);
+            MailMessage message = new MailMessage(aliasemailsendername.ToString(), _obj.ReceiverEmail);
             message.Subject = subject;
             message.Body = content;
             message.IsBodyHtml = true;
             SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new System.Net.NetworkCredential("proxyserver.myur@gmail.com", "mayu3337sis");
+            smtp.Credentials = new System.Net.NetworkCredential(emailAddress.ToString(), password.ToString());
             smtp.EnableSsl = true;
-            //smtp.Send(message);
+            smtp.Send(message);
         }
 
         public static void SendMeetingEmail(MeetingSchedular _obj, string uri, string subject, string content)
         {
-            MailMessage message = new MailMessage("proxyserver.myur@gmail.com", _obj.ReceiverEmail);
+            MailMessage message = new MailMessage(aliasemailsendername.ToString(), _obj.ReceiverEmail);
             message.Subject = subject;
             message.Body = content;
             message.IsBodyHtml = true;
             SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            smtp.Credentials = new System.Net.NetworkCredential("proxyserver.myur@gmail.com", "mayu3337sis");
+            smtp.Credentials = new System.Net.NetworkCredential(emailAddress.ToString(), password.ToString());
             smtp.EnableSsl = true;
-            //smtp.Send(message);
+            smtp.Send(message);
         }
 
     }
