ccac9761714bbf409bb2ee7e8e236d7b7195bdaa
myurnaikwadi <mayunaikwadi@gmail.com>
2016-08-26 10:11:39 +0530

    Added Missing file
    StateHomeController.js

---
+++ KindleSpurTest/KindleSpur.WebApplication/Scripts/Angular/Controller/Home/ksStateHomeController.js
@@ -0,0 +1,30 @@
+﻿app.controller('ksStateHomeController', function ($rootScope, $scope, serverCommunication, $interval, $state) {
+   // window.cocc = $scope;
+    console.error('Application Home controller --- ');
+    //$scope.bottomStrip = { style : '' };
+    $scope.invitation = {};
+    $scope.sendInvitation = function () {
+        console.error($scope.invitation)
+        if ($scope.invitation.email == '') {
+            return;
+        }
+        serverCommunication.sendInvitationToFriend({
+            invitation: { Email: [$scope.invitation.email], UserDetails: { FirstName: $rootScope.loggedDetail.FirstName, LastName: $rootScope.loggedDetail.LastName, EmailAddress: $rootScope.loggedDetail.EmailAddress }, Description: $scope.invitation.description },
+            successCallBack: function (iObj) {
+                console.error('In successCallBack', iObj);
+                $scope.invitation = {};
+            },
+            failureCallBack: function (iObj) {
+            }
+        });
+    };
+    $scope.uiFlag = { loadRepository: false, loadBottomContain : false};
+    $rootScope.$on("refreshStateHomeView", function (event,iObj) {
+        console.error('refreshStateHomeView ---- ', iObj);
+        switch (iObj.type) {
+            case "refreshUI": $scope.uiFlag.loadRepository = true; break;
+            case "loadBottomContain": $scope.uiFlag.loadBottomContain = true; break;
+        }
+        
+    });
+});
